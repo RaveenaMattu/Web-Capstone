573f41c190157d12e54e836af397b4ff51a5c3b7
Raveena Mattu <Raveena.Mattu@triosstudent.com>
2025-06-09 02:19:47 -0400

    Added student_dashboard.php

---
+++ student_dashboard.php
@@ -0,0 +1,32 @@
+
+<?php
+session_start();
+
+require('database.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Learning Pod - Login Form</title>
+  <link rel="stylesheet" href="css/app.css" />
+</head>
+<body>
+  <div class="logo"></div>
+  <main>
+  </main>
+  <footer>
+    © 2025 SMART Learning Pod by Raveena Mattu. All Rights Reserved.
+  </footer>
+    <script>
+    document.querySelectorAll('.role').forEach(role => {
+      role.addEventListener('click', function () {
+        document.querySelectorAll('.role').forEach(r => r.classList.remove('active'));
+        this.classList.add('active');
+      });
+    });
+  </script>
+</body>
+</html>
